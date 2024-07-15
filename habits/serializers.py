5b7e7c548ac5153f1b1c9b0ddf7bca8c755ee1d3
Sergey Korolev <bongrichard@rambler.ru>
2024-07-15 21:18:18 +0300

    Валидация на - Нельзя выполнять привычку реже, чем 1 раз в 7 дней.

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from habits.models import Habit
-from habits.validators import SimultaneousSelection, validate_duration, ConnectionHabitCheck, NiceHabitRewardCheck
+from habits.validators import SimultaneousSelection, validate_duration, ConnectionHabitCheck, NiceHabitRewardCheck, periodicity_check
 
 
 class HabitSerializer(ModelSerializer):
     duration = serializers.IntegerField(validators=[validate_duration])
+    periodicity = serializers.IntegerField(validators=[periodicity_check])
 
     class Meta:
         model = Habit
