b5aeda9c014de5997e73f6bacb0385f527ced848
Sergey Korolev <bongrichard@rambler.ru>
2024-07-15 23:15:12 +0300

    Каждый пользователь имеет доступ только к своим привычкам по механизму CRUD. + Пользователь может видеть список публичных привычек без возможности их как-то редактировать или удалять

--- habits/views.py
+++ habits/views.py
@@ -1,5 +1,7 @@
 
 from rest_framework.viewsets import generics
+from rest_framework.permissions import IsAuthenticated
+from habits.permissions import IsOwner
 from habits.paginations import HabitsPagination
 from habits.models import Habit
 from habits.serializers import HabitSerializer
@@ -7,6 +9,8 @@ from habits.serializers import HabitSerializer
 
 class HabitsCreateAPIView(generics.CreateAPIView):
     serializer_class = HabitSerializer
+    permission_classes = (IsAuthenticated, )
+
 
     def perform_create(self, serializer):
         habit = serializer.save()
@@ -18,17 +22,30 @@ class HabitsListAPIView(generics.ListAPIView):
     queryset = Habit.objects.all()
     pagination_class = HabitsPagination
 
+    def get_queryset(self):
+        user = self.request.user
+        return Habit.objects.filter(owner=user)
+
+
+class HabitsPublicListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.filter(published_bool=True)
+    pagination_class = HabitsPagination
+
 
 class HabitsRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+    permission_classes = (IsAuthenticated, IsOwner)
 
 
 class HabitsUpdateAPIView(generics.UpdateAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+    permission_classes = (IsAuthenticated, IsOwner)
 
 
 class HabitsDestroyAPIView(generics.DestroyAPIView):
     queryset = Habit.objects.all()
+    permission_classes = (IsAuthenticated, IsOwner)
 
